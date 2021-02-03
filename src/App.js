2fae75094a8fb70f3b87f50514153c195032c905
AwesomeDracula <trungku32@gmail.com>
2021-02-03 19:27:19 +0700

    initial app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [input, setInput] = useState('');
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      
+    </div>
+  );
+}
+
+export default App;
